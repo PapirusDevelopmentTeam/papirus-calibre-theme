73027713f919ccb91e08fd050cf0e19ae621d54f
Sergei Eremenko <finalitik@gmail.com>
2017-11-16 15:15:00 +0200

    Reduce SVG icons to 32px with several changes #3

--- imgsrc/_convert-to-png.sh
+++ imgsrc/_convert-to-png.sh
@@ -4,9 +4,10 @@ set -e
 
 SCRIPT_DIR=$(dirname "$0")
 TARGET_DIR="$SCRIPT_DIR/../resources/images"
+ZOOM_FACTOR=4
 
 echo "=> Generate bitmap icons ..."
-find "$SCRIPT_DIR/." -type f -name '*.svg' | while read file; do
+find "$SCRIPT_DIR/." -name '*.svg' | while read -r file; do
     src_dir=$(dirname "$file")
     sub_dir=$(basename "$src_dir")
     base_name=$(basename --suffix=".svg" "$file")
@@ -17,5 +18,5 @@ find "$SCRIPT_DIR/." -type f -name '*.svg' | while read file; do
     echo "Convert $file -> $dest_file"
 
     mkdir -p "$dest_dir"
-    rsvg-convert -f png "$file" -o "$dest_file"
+    rsvg-convert -z "$ZOOM_FACTOR" -f png "$file" -o "$dest_file"
 done
